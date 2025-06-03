cccaed6b229f3763b1c82f63a9d33af8aba4484f
Oscar Piña Suarez <2006ospi@gmail.com>
2025-06-03 15:49:27 +0200

    Modificacion 1 realizada

--- Ejemplo17.java
+++ Ejemplo17.java
@@ -17,5 +17,22 @@ public class Ejemplo17 {
 	} while(i <= 6);
 	
 	}
+	public static boolean dadoExtra(){
+		int dado1 = (int) (Math.random()*6);
+		int dado2 = (int) (Math.random()*6);
 
+		System.out.println("Dado 1: " + dado1);
+		System.out.println("Dado 2: " + dado2);
+		
+		if(dado1 == dado2){
+			System.out.println("Como las tiradas de ambos dados han sido iguales, se tira un tercero: ");
+			int dado3 = (int) (Math.random()*6);
+			System.out.println("Dado 3: " + dado3);
+			return true;
+		}
+
+		System.out.println("Las tiradas no han sido iguales.");
+		return false;
+	}		
 }
+	
