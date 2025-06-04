f218c71e2aadd669c0245f35285d5fd6ebfc0aac
Alex <alejandro.tallon97@gmail.com>
2025-06-04 15:39:18 +0200

    Modificacion 3 realizada

--- Ejemplo17.java
+++ Ejemplo17.java
@@ -1,21 +1,29 @@
-
 public class Ejemplo17 {
-	
-	public static void main(String[] args) {
-		
-	// Simulaicon de un dado
-		
-	int i = 1;
-	int numDado;
-	
-	do {
-		
-		numDado = (int) (Math.random()*6+1);
-		System.out.println("Ha salido el número: " + numDado);
-		i++;
-	
-	} while(i <= 6);
-	
-	}
 
+    public static void main(String[] args) {
+
+    // Simulaicon de un dado
+
+    int i = 1;
+    int numDado;
+    
+    
+    do {
+
+            numDado = (int) (Math.random()*6+1);
+            System.out.println("Ha salido el número: " + numDado);
+            i++;
+
+    } while(i <= 6);
+
+    }
+    
+    
+    
+    public static int tirard20() {
+    	int d20 = (int) (Math.random()*20+1);
+    	
+    	return d20;
+    }
 }
+
